aeb0e2ff02672eafa96575b6dd474a93613a84b8
JosemaUdeM <jose.megadeth@hotmail.com>
2016-03-13 15:10:13 -0500

    instrucciones del repositorio

---
+++ README.py
@@ -0,0 +1,19 @@
+# rsa
+codigo rsa
+
+
+
+
+#CODIGO RSA HECHO EN PYTHON POR:
+
+-JOSÉ MANUEL VERGARA ÁLVAREZ
+-ESTUDIANTE DE COMPUTACION CIENTIFICA 
+-UNIVERSIDAD DE MEDELLIN 
+-MARZO, 2016
+
+
+myrsa:
+-Codigo RSA para generar la llave publica y la llave privada dados dos numeros primos.
+
+hackrsa:
+-Codigo para decodificar un mensaje de cuatro digitos.
